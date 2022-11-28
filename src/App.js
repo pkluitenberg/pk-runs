cc007389ff5d8f24d6b959cb5f6a90a565ab36cc
Paul Kluitenberg <paul.kluitenberg@gmail.com>
2022-11-28 22:38:06 +0100

    adding email icon

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { getFeetFromMeters, getHoursFromSeconds, getMilesFromMeters } from "./St
 import InstagramIcon from '@mui/icons-material/Instagram';
 import GithubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 const App = () => {
   const [activities, setActivities] = useState([]);
@@ -147,6 +148,7 @@ const App = () => {
       <Container maxWidth={false} className="footer-container">
         <div className="footer">
           <span> &copy; Paul Kluitenberg</span>
+          <Link href="mailto:paul.kluitenberg@gmail.com" className="footer-link"><EmailIcon className="icons" /></Link>
           <Link href="https://github.com/pkluitenberg" className="footer-link"><GithubIcon className="icons" /></Link>
           <Link href="https://instagram.com/d_townpaul" className="footer-link"><InstagramIcon className="icons" /></Link>
           <Link href="https://www.linkedin.com/in/paulkluitenberg/" className="footer-link"><LinkedInIcon className="icons" /></Link>
